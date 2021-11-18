74661d3095e7bf4dc726f66e66ae4b772b1eda58
Ricardo <ricklokor1@gmail.com>
2021-11-18 19:50:40 -0300

    Commit Inicial

---
+++ src/br/com/todolist/controller/IndexController.java
@@ -0,0 +1,5 @@
+package br.com.todolist.controller;
+
+public class IndexController {
+	
+}
---
+++ src/br/com/todolist/model/StatusTarefa.java
@@ -0,0 +1,5 @@
+package br.com.todolist.model;
+
+public enum StatusTarefa {
+	ABERTA, ADIADA, CONCLUIDA, POUCO, MUITO, SUPER;
+}
